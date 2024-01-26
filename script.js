6215e03edd2c6e8676f7491a9543b9ed07f4cae0
Nirmal-P-Dev <nirmalniru1123@gmail.com>
2024-01-26 11:07:30 +0530

    new1

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ request.open("GET","https://restcountries.com/v3.1/all");
 request.send();
 request.onload = function(){
     var res = JSON.parse(request.response);
-    console.log(res);
+    console.log(req);
 }

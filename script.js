37c21afc405092822d1c992e1674552a8df2fe4f
Nirmal-P-Dev <nirmalniru1123@gmail.com>
2024-01-26 11:14:31 +0530

    new3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all");
 request.send();
-request.onload = function() {
+request.onload = function(){
     var res = JSON.parse(request.response);
-    console.log(req);
+    console.log(request);
 }
